fa397784edfa0e38f794635c77b00c234422ca91
Ruslan Curbanov <rcu@oerag.de>
2021-03-04 13:21:56 +0100

    draft proxy page with tabs

--- app/components/proxy/proxy.js
+++ app/components/proxy/proxy.js
@@ -1,3 +1,29 @@
+const defaultTab = "interceptor";
+if (!top.proxy) {
+    top.proxy = { tab: defaultTab };
+} else if (!top.proxy.tab) {
+    top.proxy.tab = defaultTab;
+}
+
+const openTab = (tab) => {
+    const tabLinks = document.getElementsByClassName("tablink");
+    for (let i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].classList.remove("active");
+    }
+
+    const tabContent = document.getElementsByClassName("tabcontent");
+    for (let i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+
+    top.proxy.tab = tab;
+    document.getElementById(tab + "-tablink").classList.add("active");
+    document.getElementById(tab).style.display = "block";
+};
+
+console.log(top.proxy.tab);
+openTab(top.proxy.tab);
+
 document.getElementById("start-proxy").addEventListener("click", async () => {
     await top.electron.startProxy();
 });
