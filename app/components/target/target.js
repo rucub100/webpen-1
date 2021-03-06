5d15cebe5e3e53358b94d0e32129b4fa1863a7b7
Ruslan Curbanov <rucub100@hhu.de>
2021-03-06 21:09:45 +0100

    draft ui components

---
+++ app/components/target/target.js
@@ -0,0 +1,24 @@
+const defaultTab = "sitemap";
+if (!top.target) {
+    top.target = { tab: defaultTab };
+} else if (!top.target.tab) {
+    top.target.tab = defaultTab;
+}
+
+const openTab = (tab) => {
+    const tabLinks = document.getElementsByClassName("tablink");
+    for (let i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].classList.remove("active");
+    }
+
+    const tabContent = document.getElementsByClassName("tabcontent");
+    for (let i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+
+    top.target.tab = tab;
+    document.getElementById(tab + "-tablink").classList.add("active");
+    document.getElementById(tab).style.display = "flex";
+};
+
+openTab(top.target.tab);
